fde1fb9a93233f29cef93d5a9b5265ac37a1cabc
hillsdej <hillsdej@uni.coventry.ac.uk>
2018-03-23 19:26:15 +0000

    fixed DFS

--- DFStesting.py
+++ DFStesting.py
@@ -57,43 +57,65 @@ class Graph:
             print(key + str(self.nodes[key].related))
 
     def dfs(self, n, route, u, limit):
+        print("----------------------------------------------------------------------------------------")
         print("this is n: " + str(n))
         print("this is u: " + str(u.name))
         u.colour = "gray"
         route.append(u)
-        print("this is the length of the path: " + str(len(route)))
+        j = 0
+        while j < len(route):
+            print(route[j].name)
+            j += 1
+
+        #print("this is the length of the path: " + str(len(route)))
         if n < limit:
             connected = []
+            print("this is u when after n < limit: " + u.name)
             for i in u.related:
+                print("this is the name when it enters the for: " + u.name)
+                #print(g.nodes[i].name)
                 print(g.nodes[i].name)
                 connected.append(g.nodes[i])
+                
             i = 0
             #print("this is done before assignment: " + str(done))
             done = False
-            print("this is done after assignment: " + str(done))
+         #   print("this is done after assignment: " + str(done))
 
             while i < len(connected) and not done:
-                print("entered for sure")
+                print("this is the name in the while loop " + u.name)
+                print(connected[i].name + " " + connected[i].colour)
+          #      print("entered for sure")
                 if connected[i].colour == "white":
+                    print("entered if with " + u.name + " as u" )
                     print("this is the connected node: " + connected[i].name + " " + connected[i].colour)
                     done = self.dfs(n+1, route, connected[i], limit)
                 
                 i += 1
-                print("this is i: " + str(i))
+           #     print("this is i: " + str(i))
 
                         
 
-            print(done)
+                #print(done)
             if not done:
-                print("entering?")
-                route.pop()
+                #   print("entering?")
+                # for k in route:
+                #     route[i].colour = "white"
+                u = route.pop()
+                print("this is the route after its been popped: " + str(len(route)))
+                print("this is where it restarts " + str(u.name))
                 u.colour = "white"
-                n = 1
+                #self.dfs(0, route, u, limit)
         else:
             done = True
 
-        
-        
+
+        print("------------------------>>>>>>>>this is the status of done: " + str(done))
+        x = 0 
+        while x < len(route):
+            print(route[x].name)
+            x += 1
+      
         return done
         #return len(route)
     
@@ -111,12 +133,13 @@ for i in range(0,12):
     g.add_node(Node(sample_size[i]))
 
 edges = ['AG','AF','AI','BC','BH','BJ','CI','CK','DJ','DL','EG','EK','FH','FL','GL','JK']
+#edges = ['AB', 'AD', 'BC', 'BD', 'DE', 'EF', 'FC']
 
 for edge in edges:
     g.add_edge(edge[:1], edge[1:])
 
 route = [] 
-nodes = g.dfs(0,route,g.nodes["A"],12)
+nodes = g.dfs(1,route,g.nodes["A"],12)
 print(nodes)
 # with open("dfs_output.txt", "w") as f:
 # for i in nodes:
