7d314fcba37760fe1109fb2cb360e5741c346635
hillsdej <hillsdej@uni.coventry.ac.uk>
2018-03-27 21:29:48 +0100

    working automated graph nodes and edges

--- DFStesting.py
+++ DFStesting.py
@@ -28,9 +28,16 @@ class Graph:
         #combine letters and numbers to make nodes
         #check it doesnt already exist in the dictionary
         #for letter in letters add new node + number
+        # if node.name != "A1" and node.name != "A4" and node.name != "D1" and node.name != "D4": use only if you need to prove it works
+
         if node.name not in self.nodes:
             self.nodes[node.name] = node
             self.nodes[node.colour] = node
+        
+    
+    def get_nodes(self):
+        return sorted(list(self.nodes.keys()))
+
 
     def add_edge(self,u,v):
         #needs the list of letters and numbers
@@ -79,38 +86,132 @@ class Graph:
     
 g = Graph()
 
-## examples of adding singular nodes
-## a = Node('A')
-## g.add_node(a)
-## g.add_node(Node('B'))
-## g.add_node(Node('A1'))
-
 import string
 sample_size = list(string.ascii_uppercase) #gets a list of the alphabet characters in uppercase
 letters = []
 numbers = []
+
 for x in range(0,4):
     letters.append(sample_size[x])
     numbers.append(x+1)
 
 counter = 0
 
-while counter < 4: #letter
+#add nodes
+while counter < 4: #letter (adjust to board size)
     for y in range(0,4):
         g.add_node(Node(letters[counter]+str(numbers[y])))
     counter += 1
- 
-#edges = ['AG','AF','AI','BC','BH','BJ','CI','CK','DJ','DL','EG','EK','FH','FL','GL','JK']
-    
-# for edge in edges:
-#     print(edge[:1], edge[1:])
-    # g.add_edge(edge[:1], edge[1:])
 
-g.add_edge("B4","D3")
-g.add_edge("D3","B4")
-# route = [] 
-# nodes = g.dfs(1,route,g.nodes["A"],12)
-# print(nodes)
-g.print_graph()
+#optimise this
+def downTwoLeftOne(lettersList, currentLetter, number):
+    #border cases
+    if currentLetter == "A":
+        return False
+    if number <=2:
+        return False
+    letterPosition = lettersList.index(currentLetter)
+    newLetter = lettersList[letterPosition-1]
+    number -= 2
+    return newLetter+str(number)
+
+def downTwoRightOne(lettersList, currentLetter, number):
+    if number <= 2:
+        return False
+    letterPosition = lettersList.index(currentLetter)
+    if letterPosition == len(lettersList)-1: 
+        return False
+    newLetter = lettersList[letterPosition+1]
+    number -= 2
+    return newLetter+str(number)
+
+def downOneLeftTwo(lettersList, currentLetter, number):
+    letterPosition = lettersList.index(currentLetter)
+    if letterPosition <= 1:
+        return False
+    if number < 2:
+        return False
+    newLetter = lettersList[letterPosition-2]
+    number -= 1
+    return newLetter+str(number)
+
+def downOneRightTwo(lettersList, currentLetter, number):
+    if number < 2:
+        return False
+    letterPosition = lettersList.index(currentLetter)
+    if letterPosition >= len(lettersList)-2:
+        return False
+    newLetter = lettersList[letterPosition+2]
+    number -= 1
+    return newLetter+str(number)
+
+def upTwoLeftOne(lettersList, currentLetter, number):
+    if number >= len(lettersList)-2: #1?
+        return False
+    if currentLetter == "A":
+        return False
+    letterPosition = lettersList.index(currentLetter)
+    newLetter = lettersList[letterPosition-1]
+    number += 2
+    return newLetter+str(number)
+
+def upTwoRightOne(lettersList, currentLetter, number):
+    if number >= len(lettersList)-1: #1?
+        return False
+    letterPosition = lettersList.index(currentLetter)
+    if letterPosition >= len(lettersList)-1:
+        return False
+    newLetter = lettersList[letterPosition+1]
+    number += 2
+    return newLetter+str(number)
+
+def upOneLeftTwo(lettersList, currentLetter, number):
+    if number >= len(lettersList):
+        return False
+    letterPosition = lettersList.index(currentLetter)
+    if letterPosition <= 1: #if we get an "A" or a "B"
+        return False
+    newLetter = lettersList[letterPosition-2]
+    number += 1
+    return newLetter+str(number)
+
+def upOneRightTwo(lettersList, currentLetter, number):
+    if number >= len(lettersList):
+        return False
+    letterPosition = lettersList.index(currentLetter)
+    if letterPosition >= len(lettersList)-2:
+        return False
+    newLetter = lettersList[letterPosition+2]
+    number += 1
+    return newLetter+str(number)
+
+nodeList = g.get_nodes()
+
+#shorten this somehow
+for n in range(0, len(nodeList)-1):
+    if downTwoLeftOne(letters, nodeList[n][:1], int(nodeList[n][1:])) != False:
+        g.add_edge(nodeList[n], downTwoLeftOne(letters, nodeList[n][:1], int(nodeList[n][1:])))
+    if downTwoRightOne(letters, nodeList[n][:1], int(nodeList[n][1:])) != False:
+        g.add_edge(nodeList[n], downTwoRightOne(letters, nodeList[n][:1], int(nodeList[n][1:])))
+    if downOneLeftTwo(letters, nodeList[n][:1], int(nodeList[n][1:])) != False:
+        g.add_edge(nodeList[n], downOneLeftTwo(letters, nodeList[n][:1], int(nodeList[n][1:])))
+    if downOneRightTwo(letters, nodeList[n][:1], int(nodeList[n][1:])) != False:
+        g.add_edge(nodeList[n], downOneRightTwo(letters, nodeList[n][:1], int(nodeList[n][1:])))
+   
+    if upTwoLeftOne(letters, nodeList[n][:1], int(nodeList[n][1:])) != False:
+        g.add_edge(nodeList[n], upTwoLeftOne(letters, nodeList[n][:1], int(nodeList[n][1:])))
+    if upTwoRightOne(letters, nodeList[n][:1], int(nodeList[n][1:])) != False:
+        g.add_edge(nodeList[n], upTwoRightOne(letters, nodeList[n][:1], int(nodeList[n][1:])))
+    if upOneLeftTwo(letters, nodeList[n][:1], int(nodeList[n][1:])) != False:
+        g.add_edge(nodeList[n], upOneLeftTwo(letters, nodeList[n][:1], int(nodeList[n][1:])))
+    if upOneRightTwo(letters, nodeList[n][:1], int(nodeList[n][1:])) != False:
+        g.add_edge(nodeList[n], upOneRightTwo(letters, nodeList[n][:1], int(nodeList[n][1:])))
+
+g.print_graph() #figure out how to get rid of the white key
+route = [] 
+nodes = g.dfs(1,route,g.nodes["A1"],16) # eventually change them to variables such as start node and number of nodes/size of board
+print(nodes) 
+
+
 
 
